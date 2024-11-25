a4b5ecc57c3751530add394bbe72c3a13b4ac682
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-11-25 15:01:10 +0200

    Saves

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,18 +5,21 @@ public class CameraScript : MonoBehaviour
 {
     private Transform character;
     private InputAction lookAction;
-    private Vector3 cameraAngles;
+    private Vector3 cameraAngles, cameraAngles0;
     private Vector3 r;
     private float sensitivityH = 5.0f;
     private float sensitivityV = -3.0f;
     private float minFpvDistance = 0.9f;
+    private float maxFpvDistance = 9.0f;
+    private bool isPos3;
 
     void Start()
     {
         lookAction = InputSystem.actions.FindAction("Look");
-        cameraAngles = this.transform.eulerAngles;
+        cameraAngles0 = cameraAngles = this.transform.eulerAngles;
         character = GameObject.Find("Character").transform;
         r = this.transform.position - character.position;
+        isPos3 = false;
     }
 
     void Update()
@@ -24,36 +27,54 @@ public class CameraScript : MonoBehaviour
         Vector2 wheel = Input.mouseScrollDelta;
         if(wheel.y != 0)
         {
-            if(r.magnitude >= minFpvDistance)
+            if (r.magnitude >= maxFpvDistance)
             {
-                float rr = r.magnitude * (1 - wheel.y / 10);
-                if (rr <= minFpvDistance)
+                isPos3 = true;
+                if (wheel.y > 0)
                 {
-                    r *= 0.01f;
-                }
-                else
-                {
-                    r *= (1 - wheel.y / 10);
+                    r *= 1 - wheel.y / 10;
                 }
             }
             else
             {
-                if(wheel.y < 0)
+                isPos3 = false;
+                if (r.magnitude >= minFpvDistance)
                 {
-                    r *= 100f;
+                    float rr = r.magnitude * (1 - wheel.y / 10);
+                    if (rr <= minFpvDistance)
+                    {
+                        r *= 0.01f;
+                    }
+                    else
+                    {
+                        r *= (1 - wheel.y / 10);
+                    }
+                }
+                else
+                {
+                    if (wheel.y < 0)
+                    {
+                        r *= 100f;
+                    }
                 }
             }
         }
-
-        Vector2 lookValue = lookAction.ReadValue<Vector2>();
-        if(lookValue != Vector2.zero)
+        if (!isPos3)
         {
-            cameraAngles.x += lookValue.y * Time.deltaTime * sensitivityV;
-            cameraAngles.y += lookValue.x * Time.deltaTime * sensitivityH;
-            this.transform.eulerAngles = cameraAngles;
+            Vector2 lookValue = lookAction.ReadValue<Vector2>();
+            if (lookValue != Vector2.zero)
+            {
+                cameraAngles.x += lookValue.y * Time.deltaTime * sensitivityV;
+                cameraAngles.y += lookValue.x * Time.deltaTime * sensitivityH;
+                this.transform.eulerAngles = cameraAngles;
+            }
+            this.transform.position = character.position +
+                Quaternion.Euler(
+                    cameraAngles.x - cameraAngles0.x, 
+                    cameraAngles.y - cameraAngles0.y, 
+                    0
+                ) * r;
         }
-        this.transform.position = character.position + 
-            Quaternion.Euler(0, cameraAngles.y, 0) * r;
     }
 }
 /* Обмежити величину зміни кута камери по вертикалі (у бік горизонту)
@@ -63,4 +84,8 @@ public class CameraScript : MonoBehaviour
  *  
  * Обмежити величину максимального віддалення камери від персонажу
  *  - підібрати практично за відстаню, з якої видно все поле
+ *  \
+ *  
+ *  Д.З. Підібрати текстуру неба (Skybox)
+ *  Розмістити по сцені декілька джерел точкового світла.
  */
