ef051770d91d6350d2037c619406d96896dc9bf8
dns-lector <157026463+dns-lector@users.noreply.github.com>
2024-12-02 17:47:41 +0200

    Saves

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameState
@@ -5,4 +7,29 @@ public class GameState
     public static bool isDay { get; set; }
     public static bool isFpv { get; set; }
 
+    #region Game events
+    // Emit Signal Trigger Dispatch [Event]
+    public static void TriggerGameEvent(String eventName, )
+    {
+        foreach (var action in subscribers[eventName])
+        {
+            action(eventName);
+        }
+    }
+
+    private static Dictionary<string, List<Action<String>>> subscribers = new();
+    public static void Subscribe(Action<String> action, String eventName)
+    {
+
+    }
+    public static void Unsubscribe(Action<String> action, String eventName)
+    {
+
+    }
+    #endregion
 }
+/* Д.З. Реалізувати процес розряджання "батарейок" - якщо їх тривалий
+ * час не збирають, то їх заряд зменшується.
+ * - додати індикатор залишкового заряду
+ * - реалізувати можливість зазначення часу розряду кожній батарейці окремо.
+ */
